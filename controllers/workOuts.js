d3a35f588cdc38f0e202221aad7a51797fd8a169
djrrivera <djrrivera25@gmail.com>
2025-06-19 11:37:14 +0800

    fix response

--- controllers/workOuts.js
+++ controllers/workOuts.js
@@ -18,9 +18,7 @@ export const addWorkOut = async (req, res) => {
       duration,
     });
     await newWorkout.save();
-    return res.status(201).json({
-      message: `new Workout named: ${name} is successfully added`,
-    });
+    return res.status(201).json(newWorkout);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error.message });
